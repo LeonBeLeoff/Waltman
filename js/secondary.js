21a48c2297d792ee4b2130f3be14c5e51b91c1c7
LeonBeLeoff <leon.bagger.torang@student.tabyenskilda.se>
2022-05-07 21:10:56 +0200

    Finished Base fr fr

--- js/secondary.js
+++ js/secondary.js
@@ -1,31 +1,3 @@
-/* S l i d e   s h o w */
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-
-
 /* D r o p d o w n s */
 
 // Menu drop down ----------------------------------------
